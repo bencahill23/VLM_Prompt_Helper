bf1e95d57940717da0fa96b009f002e706390107
Ben Cahill <70894199+bencahill23@users.noreply.github.com>
2025-09-10 08:56:10 +0200

    Updated with secrets and config

--- promptHelper.py
+++ promptHelper.py
@@ -3,27 +3,23 @@ import json
 import pyperclip as pc
 from google import genai
 
-#from secrets import secrets
 
 #TODO 
-# Fix secrets
-# implement Clear button
+
 # implement chunking on text outputs
-#
+# implement evaluation-chat
+# implement model change
 
 @st.cache_resource
-def initAI():
-	#googleapikey = secrets.secrets('GOOGLE_API_KEY')
-	googleapikey ="AIzaSyBC6tI_A5paMPTTAJt9OU1V25Ussf9iCeo"
-	global client
+def initAI(googleapikey):
 	client = genai.Client(api_key=googleapikey)	
 	return client
 
 @st.cache_data
 def loadPrompts(path):
-	# Open and read the JSON file
 	with open(path, 'r') as promptsfile:
 		promptlist = json.load(promptsfile)
+
 	promptsfile.close()
 	return promptlist
 
@@ -31,7 +27,7 @@ def clipCopy():
 	pc.copy(str(st.session_state['newPrompt']))
 
 def clearPrompt():
-	st.session_state['currentPrompt'] = ''
+	st.session_state['currentPrompt'] = ""
 
 @st.cache_data
 def getFileNames():
@@ -69,116 +65,152 @@ def doNewPrompt(prompttext, responsetext, struct, sum, env):
 	return newresponse.text
 
 def prompt_change():
-	#print(st.session_state["prompt_box"])
-	#st.session_state['currentPrompt']
-	pass
+	for prompt in promptlist:
+		if prompt['DisplayName'] == st.session_state["prompt_box"]:
+			st.session_state['currentPrompt'] = prompt['Prompt']
 
-# Set session variables
-if 'LLMEval' not in st.session_state:
-	st.session_state['LLMEval'] = ''
+# Init session variables 
+def initSession():
+	session_variables = ['LLMEval', 'currentPrompt', 'newPrompt']
+	for sessvar in session_variables:
+		if sessvar not in st.session_state:
+			st.session_state[sessvar] = ''
 
-if 'currentPrompt' not in st.session_state:
-	st.session_state['currentPrompt'] = ''
-
-if 'currentPromptSel' not in st.session_state:
-	st.session_state['currentPromptSel'] = ''
-
-if 'newPrompt' not in st.session_state:
-	st.session_state['newPrompt'] = ''
+	populatePromptLists()
 
 
-client = initAI()
+def populatePromptLists():
+	global promptlist
+	global promptnames
+	# Global Prompt Library
+	promptlist=loadPrompts('assets/Prompts.json')
+	promptnames = [] 
+	for prompt in promptlist:
+		promptnames.append(prompt['DisplayName'])
+
+
+def saveNewPrompt(newprompt, newdisplayname):
+	new_data = {
+    "IsScheduledOnly": False,
+    "DisplayName": newdisplayname,
+    "Prompt": newprompt
+    }
+	path = 'assets/Prompts.json'
+
+	with open(path, "r") as file:
+		data = json.load(file)  # Load JSON data into a Python object
+
+	# Step 2: Append new data
+	if isinstance(data, list):  # If the JSON is a list
+		data.append(new_data)
+	elif isinstance(data, dict):  # If the JSON is a dictionary
+		data.update(new_data)
+	else:
+		print("no format match for JSON")
+
+	# Step 3: Write the updated data back to the file
+	with open(path, "w") as file:
+		json.dump(data, file, indent=4)  # Save with pretty formatting
+	file.close()
+	populatePromptLists()
+
+
+
+
+initSession()
+populatePromptLists()
+
+googleapikey = st.secrets["GOOGLE_API_KEY"]
+client = initAI(googleapikey)
  
 # list available models
 eval_models = ["GPT-5", "Claude", "Gemini"]
-
 lorem = "THIS IS THE VLM RESPONSE!"
-# Open and read the JSON file
-
-promptlist=loadPrompts('assets/Prompts.json')
-# Get prompt DisplayNames into list for menus
-promptnames = [] 
-for prompt in promptlist:
-	promptnames.append(prompt['DisplayName'])
 
 
 
-# Start GUI
 
-title = st.title("PromptHelper")
+
+################
+#  Start GUI
+################
+title = st.title("VLM Prompt-Helper")
+tab1, tab2 = st.tabs(["VLM Prompting Sandbox", "Prompt Assistant"])
+
+################
 # VLM Section
-with st.container(border=True):
-	clipnames = getFileNames()
-	st.header("VLM Clips Sandbox")
-	st.text("Select a Prompt from the library, or create your own and save it.")
-	prompt_selection = st.selectbox("Prompt Library", promptnames, key="prompt_box", on_change = prompt_change)
+################
+with tab1:
+	with st.container(border=True):
+		clipnames = getFileNames()
+		st.header("VLM Clips Sandbox")
+		st.text("Select a Prompt from the library, or create your own and save it.")
+		col1,col2 = st.columns([1,1], vertical_alignment="top")
+		with col1: 
+			clip_selection = st.selectbox("Clip Library", clipnames)
+			prompt_selection = st.selectbox("Prompt Library", promptnames, key="prompt_box", on_change = prompt_change)
+		with col2:
+			st.image('assets/traffic.jfif', width="stretch")
 
-	for prompt in promptlist:
-		if prompt['DisplayName'] == prompt_selection:
-			promptcontent = prompt['Prompt']
-			if promptcontent:
-				st.session_state['currentPrompt'] = promptcontent
-			
+		prompt_text = st.text_area("Enter your prompt for VLM (Finish with Ctrl+Enter)", value=st.session_state['currentPrompt'], height="content")
 
-	prompt_text = st.text_area("Enter your prompt for evaluation", value=st.session_state['currentPrompt'], height="content")
+		if prompt_text:
+			st.session_state['currentPrompt'] = prompt_text
 
-	if prompt_text:
-		st.session_state['currentPrompt'] = prompt_text
+		col11, col12, col13 = st.columns([1,1,1],vertical_alignment="bottom")
+		with col11:
+			clear_button = st.button("Clear Prompt Text", width="stretch", on_click=clearPrompt)
+		with col12:
+			save_prompt_button = st.button("Save Prompt As...", width="stretch", on_click=saveNewPrompt, args=["This is a new prompt", "New Prompt Title"])
+		with col13:
+			save_prompt_button = st.button("Overwrite Prompt", width="stretch")
 
-	col11, col12, col13 = st.columns([1,1,1],vertical_alignment="bottom")
-	with col11:
-		clear_button = st.button("Clear Prompt Text", width="stretch", on_click=clearPrompt)
-	with col12:
-		save_prompt_button = st.button("Save Prompt As...", width="stretch")
-	with col13:
-		save_prompt_button = st.button("Overwrite Prompt", width="stretch")
+		send_to_vlm_button = st.button("Send Clip and Prompt to VLM", width="stretch")
 
-	col1,col2 = st.columns([1,2], vertical_alignment="top")
-	with col1: 
-		clip_selection = st.selectbox("Select Clip", clipnames)
+		if(send_to_vlm_button):
+			st.text_area("VLM Output", value=lorem, height="content")
 
-	with col2:
-		st.image('assets/traffic.jfif', width="stretch")	
-
-	send_to_vlm_button = st.button("Send Clip and Prompt to VLM", width="stretch")
-
-	if(send_to_vlm_button):
-		st.text_area("VLM Output", value=lorem, height="content")
-
+################
 # LLM Prompt Evaluation Section
-with st.container(border=True):
-	st.header("Prompt Assistant")
-	col1, col2 = st.columns([1,1],vertical_alignment="bottom")
-	with col1:
-		model_selection = st.selectbox("Model", eval_models)
-	with col2:	
-		submit_button = st.button("Evaluate Prompt", width="stretch")
+################
+with tab2:
+	with st.container(border=True):
+		st.header("Prompt Assistant")
+		with st.expander("Your Prompt"):
+			# = st.text_area("Prompt to be evaluated",label_visibility="collapsed", value=st.session_state['currentPrompt'], height="content")
+			prompt_text_repro = st.write(st.session_state['currentPrompt'])
 
-	if(submit_button):
-		LLMEval = doPromptEval(st.session_state['currentPrompt'])
-		if LLMEval:
-			st.session_state['LLMEval'] = LLMEval
+		col1, col2 = st.columns([1,1],vertical_alignment="bottom")
+		with col1:
+			model_selection = st.selectbox("Model", eval_models)
+		with col2:	
+			submit_button = st.button("Evaluate Prompt", width="stretch")
 
-	try:
-		st.text_area("Evaluation", value=st.session_state['LLMEval'], height="content")
-		chatprompt = st.chat_input(placeholder="Ask about the prompt", width="stretch")
-	except NameError:
-		pass
-	col1, col2, col3 = st.columns([1,1,1],vertical_alignment="bottom")
-	with col1:
-		structure_output = st.checkbox("Ask for Structured Output")
-	with col2:
-		summary_output = st.checkbox("Ask for Summarized Output")
-	with col3:
-		environment_output = st.checkbox("Include Environment")
+		if(submit_button):
+			LLMEval = doPromptEval(st.session_state['currentPrompt'])
+			if LLMEval:
+				st.session_state['LLMEval'] = LLMEval
+
+		try:
+			st.text_area("Evaluation", value=st.session_state['LLMEval'], height="content")
+			chatprompt = st.chat_input(placeholder="Ask about the prompt (not implemented)", width="stretch")
+		except NameError:
+			pass
+		col1, col2, col3 = st.columns([1,1,1],vertical_alignment="bottom")
+		with col1:
+			structure_output = st.checkbox("Ask for Structured Output")
+		with col2:
+			summary_output = st.checkbox("Ask for Summarized Output")
+		with col3:
+			environment_output = st.checkbox("Include Environment")
 
 
 
-	make_new_prompt_button = st.button("Generate New Prompt", width="stretch")
-	if (make_new_prompt_button):
-		newptext=doNewPrompt(st.session_state['currentPrompt'], st.session_state['LLMEval'], structure_output, summary_output, environment_output)
-		if(newptext):
-			st.session_state['newPrompt'] = newptext
-		st.text_area("Suggested New Prompt", value= st.session_state['newPrompt'], height="content")
-		copy_button = st.button("Copy to Clipboard", width="stretch", on_click=clipCopy)
+		make_new_prompt_button = st.button("Generate New Prompt", width="stretch")
+		if (make_new_prompt_button):
+			newptext=doNewPrompt(st.session_state['currentPrompt'], st.session_state['LLMEval'], structure_output, summary_output, environment_output)
+			if(newptext):
+				st.session_state['newPrompt'] = newptext
+			st.text_area("Suggested New Prompt", value= st.session_state['newPrompt'], height="content")
+			copy_button = st.button("Copy to Clipboard", width="stretch", on_click=clipCopy)
 
--- secrets.py
+++
@@ -1,1 +0,0 @@
-secrets = {'GOOGLE_API_KEY' : "AIzaSyBC6tI_A5paMPTTAJt9OU1V25Ussf9iCeo"}
